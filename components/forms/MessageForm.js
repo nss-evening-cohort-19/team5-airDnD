0cb812a12efd4f2483a9912936ef9d80e3074922
Jessica Morrison <jnicomorr@gmail.com>
2022-08-05 20:59:11 -0500

    Added msg form and path

--- components/forms/MessageForm.js
+++ components/forms/MessageForm.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function MessageForm() {
   return (
-    <div>MessageForm</div>
+    <>
+      <div className="form-floating mb-3">
+        <input type="title" className="form-control" id="floatingInput" placeholder="Title" />
+        <label htmlFor="floatingInput">Title</label>
+      </div>
+      <div className="form-floating">
+        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{ height: '100px' }} />
+        <label htmlFor="floatingTextarea2">Comments</label>
+      </div>
+    </>
   );
 }
