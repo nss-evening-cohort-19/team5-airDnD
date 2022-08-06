6f463fd428ac4e4c7c821dd0959883398e23119e
Jessica Morrison <jnicomorr@gmail.com>
2022-08-05 21:20:21 -0500

    Added edit msg code

--- pages/Profile/Messages/[firebaseKey].js
+++ pages/Profile/Messages/[firebaseKey].js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// Create Dynamic page where users can create a Message
+
 export default function firebaseKey() {
   return <div>edit</div>;
 }
--- pages/Profile/Messages/edit/[firebaseKey].js
+++ pages/Profile/Messages/edit/[firebaseKey].js
@@ -1,5 +1,21 @@
-import React from 'react';
+// import React, { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import MessageForm from '../../../../components/forms/MessageForm';
 
-export default function firebaseKey() {
-  return <div>edit</div>;
-}
+// // Create Dynamic page where users can edit their Message
+
+// export default function EditMessage() {
+//   const [editMessage, setMessage] = useState({});
+//   const router = useRouter();
+//   const { firebaseKey } = router.query;
+
+//   useEffect(() => {
+//     getSingleMessage(firebaseKey).then(setMessage);
+//   }, [firebaseKey]);
+
+//   return (
+//     <div className="create-form" style={{ height: '45rem', padding: '10%' }}>
+//       <MessageForm obj={editMessage} />
+//     </div>
+//   );
+// }
