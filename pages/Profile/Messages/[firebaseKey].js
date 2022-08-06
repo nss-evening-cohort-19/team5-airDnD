df43b3a67e56ac318f5511749a59d0fc42f5ce41
Jessica Morrison <jnicomorr@gmail.com>
2022-08-05 22:09:01 -0500

    Added msg card

--- pages/Profile/Messages/[firebaseKey].js
+++ pages/Profile/Messages/[firebaseKey].js
@@ -1,7 +1,20 @@
-import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { getAllMessages } from '../../../api/messageData';
 
-// Create Dynamic page where users can create a Message
+// // Create Dynamic page where users can view the messages
+// export default function ViewMessages() {
+//   const [viewMessage, setViewMessage] = useState({});
+//   const router = useRouter();
+//   const { firebaseKey } = router.query;
 
-export default function firebaseKey() {
-  return <div>edit</div>;
-}
+//   useEffect(() => {
+//     getAllMessages(firebaseKey).then(setViewMessage);
+//   }, [firebaseKey]);
+
+//   return (
+//     <div className="view-card">
+//       <MessageCard key={firebaseKey} messageObj={viewMessage} />
+//     </div>
+//   );
+// }
