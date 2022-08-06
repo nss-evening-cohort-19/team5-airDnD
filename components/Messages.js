25bf09789c0b2eb7a21a9a7d8efcfa299279640d
Shalane Proctor <shalane088@gmail.com>
2022-08-05 22:03:33 -0500

    .

--- components/Messages.js
+++ components/Messages.js
@@ -4,16 +4,18 @@ import PropTypes from 'prop-types';
 export default function MessagesSection({ messageObj }) {
   return (
     <>
-      <p>
-        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-          <h3>Message From:</h3>
-          <h3>{messageObj.from}</h3>
-          <p>{messageObj.msgTitle}</p>
-        </button>
-      </p>
-      <div className="collapse" id="collapseExample">
-        <div className="card card-body">
-          {messageObj.message}
+      <div className="card">
+        <div className="card-body">
+          <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            <h3>Message From:</h3>
+            <h3>{messageObj.from}</h3>
+            <p>{messageObj.msgTitle}</p>
+          </button>
+          <div className="collapse" id="collapseExample">
+            <div className="card card-body">
+              {messageObj.message}
+            </div>
+          </div>
         </div>
       </div>
     </>
