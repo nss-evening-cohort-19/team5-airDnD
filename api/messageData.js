460866b2b357e13ae12a0f4ac599965e90bceaf7
Jessica Morrison <jnicomorr@gmail.com>
2022-08-05 21:34:49 -0500

    Added msg data

---
+++ api/messageData.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { clientCredentials } from '../utils/client';
+
+const dbUrl = clientCredentials.databaseURL;
+
+// Get all User Properties
+
+const getAllMessages = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/messages.json.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+// Create a User Property
+const createMessage = (messageObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/messages.json`, messageObj)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/messages/${response.data.name}.json`, payload)
+        .then(resolve);
+    }).catch(reject);
+});
+
+// Update User Property
+const updateMessage = (messageObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/messages/${messageObj.firebaseKey}.json`, messageObj)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+// Delete User Property
+const deleteMessage = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/messages/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+// Get Single property
+const getSingleMessage = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/messages/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+export {
+  createMessage,
+  getAllMessages,
+  getSingleMessage,
+  deleteMessage,
+  updateMessage,
+};
--- pages/Profile/Messages/edit/[firebaseKey].js
+++ pages/Profile/Messages/edit/[firebaseKey].js
@@ -1,21 +1,22 @@
-// import React, { useState, useEffect } from 'react';
-// import { useRouter } from 'next/router';
-// import MessageForm from '../../../../components/forms/MessageForm';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import MessageForm from '../../../../components/forms/MessageForm';
+import { getSingleMessage } from '../../../../api/messageData';
 
-// // Create Dynamic page where users can edit their Message
+// Create Dynamic page where users can edit their Message
 
-// export default function EditMessage() {
-//   const [editMessage, setMessage] = useState({});
-//   const router = useRouter();
-//   const { firebaseKey } = router.query;
+export default function EditMessage() {
+  const [editMessage, setMessage] = useState({});
+  const router = useRouter();
+  const { firebaseKey } = router.query;
 
-//   useEffect(() => {
-//     getSingleMessage(firebaseKey).then(setMessage);
-//   }, [firebaseKey]);
+  useEffect(() => {
+    getSingleMessage(firebaseKey).then(setMessage);
+  }, [firebaseKey]);
 
-//   return (
-//     <div className="create-form" style={{ height: '45rem', padding: '10%' }}>
-//       <MessageForm obj={editMessage} />
-//     </div>
-//   );
-// }
+  return (
+    <div className="create-form" style={{ height: '45rem', padding: '10%' }}>
+      <MessageForm obj={editMessage} />
+    </div>
+  );
+}
