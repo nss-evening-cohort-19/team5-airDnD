1d77a347414968785b7a571b581e837a924cd154
Shalane Proctor <shalane088@gmail.com>
2022-08-10 22:04:03 -0500

    Edited nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,56 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
-import Link from 'next/link';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import Image from 'next/image';
 import { signOut } from '../utils/auth';
+import { useAuth } from '../utils/context/authContext';
+import Dice from '../public/images/dice.png';
 
 export default function NavBar() {
+  const { user } = useAuth();
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Image src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" />
-        <Link passHref href="/">
-          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            AirDnD
-          </a>
-        </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link passHref href="/">
-                <a className="nav-link">Properties</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link passHref href="/Properties/new">
-                <a className="nav-link">Add Your Space</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link passHref href="/profile">
-                <a className="nav-link">Profile</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link passHref href="/Profile/Reservations/new">
-                <a className="nav-link">Book a Reservation</a>
-              </Link>
-            </li>
-            <li>
-              <Link passHref href="/Profile/Messages/newMsg">
-                <a className="nav-link">New Message</a>
-              </Link>
-            </li>
-            <button type="button" className="btn btn-danger" onClick={signOut}>
-              Sign Out
-            </button>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar bg="light" expand="lg" style={{ marginBottom: '1.5rem' }}>
+      <Container>
+        <Navbar.Brand href="#home" style={{ fontSize: '2rem' }}>
+          AirDnD <Image src={Dice} alt="Dice" height={40} width={40} />
+        </Navbar.Brand>
+        <Nav.Link href="/Profile/Reservations/new">Where|Check-in|Check-out|Who</Nav.Link>
+        <NavDropdown
+          title={(
+            <img className="thumbnail-image" src={user.photoURL} alt="Profile Pic" style={{ width: '30%', borderRadius: '50%' }} />
+          )}
+          id="basic-nav-dropdown"
+        >
+          <NavDropdown.Item href="/">View Listings</NavDropdown.Item>
+          <NavDropdown.Item href="/profile">Account</NavDropdown.Item>
+          <NavDropdown.Item href="/Profile/Reservations/new">New Reservation</NavDropdown.Item>
+          <NavDropdown.Item href="/Profile/Messages/new">New Message</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item onClick={signOut}>Logout</NavDropdown.Item>
+        </NavDropdown>
+      </Container>
+    </Navbar>
   );
 }
